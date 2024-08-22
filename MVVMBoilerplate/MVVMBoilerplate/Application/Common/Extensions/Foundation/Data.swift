6047ed663170fd875d9deecab1696c171a22d4da
umair-bandlab <umair.afzal@bandlab.com>
2024-08-22 21:52:49 +0500

    Initial Commit for the project

---
+++ MVVMBoilerplate/MVVMBoilerplate/Application/Common/Extensions/Foundation/Data.swift
@@ -0,0 +1,16 @@
+
+
+import Foundation
+
+extension Data {
+    
+    var html2AttributedString: NSAttributedString? {
+        do {
+            return try NSAttributedString(data: self, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            print("error:", error)
+            return  nil
+        }
+    }
+    
+}
