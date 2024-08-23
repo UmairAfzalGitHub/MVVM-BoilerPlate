0e2a83a8c7ab1e3d739cc23b42f119545f08777d
umair-bandlab <umair.afzal@bandlab.com>
2024-08-23 11:00:37 +0500

    Added multipart request

--- MVVMBoilerplate/MVVMBoilerplate/Application/Common/Extensions/Foundation/Data.swift
+++ MVVMBoilerplate/MVVMBoilerplate/Application/Common/Extensions/Foundation/Data.swift
@@ -13,4 +13,9 @@ extension Data {
         }
     }
     
+    mutating func append(_ string: String) {
+        if let data = string.data(using: .utf8) {
+            self.append(data)
+        }
+    }
 }
